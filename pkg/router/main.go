671929402c10b7ee31bbfa693d1640f921757b5b
eminoz <eminoz@yahoo.com>
2022-07-03 23:03:15 +0300

    added commit

--- pkg/router/main.go
+++ pkg/router/main.go
@@ -12,7 +12,7 @@ func Setup() *fiber.App {
 
 	//User endpoints
 	userCollection := repository.UserCollection()
-	userEntity := repository.InsertOneEntity(userCollection) //giving userCollection struct
+	userEntity := repository.InsertOneEntity(userCollection) //giving userCollection struct to generic func
 	userService := service.UserService{UserRepo: userEntity}
 	controller := api.UserController{UserServices: &userService}
 	u := f.Group("/user")
@@ -21,7 +21,7 @@ func Setup() *fiber.App {
 
 	//Orders endpoints
 	orderCollection := repository.OrderCollection()
-	orderEntity := repository.InsertOneEntity(orderCollection) //giving orderCollection struct
+	orderEntity := repository.InsertOneEntity(orderCollection) //giving orderCollection struct to generic func
 	orderService := service.OrderService{OrderRepo: orderEntity}
 	orderController := api.OrderController{OrderServices: &orderService}
 	o := f.Group("/order")
